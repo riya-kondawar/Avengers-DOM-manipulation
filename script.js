e4bdbffe7d017fba56db1e9565aa0fd376cfcd57
riiya-kondawar <riya.kondawar@gmail.com>
2024-09-26 13:22:44 +0530

    updates

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-// Avengers scripts.js file
+// DOM: Avengers scripts.js file
 
 
-// Function to change the title text using innerHTML
+// Change the title text
 function changeText() {
     const title = document.getElementById("title");
     title.innerHTML = "Assemble! The Avengers Are Here!";
 }
 
-// Function to highlight Avengers by changing the color of text using getElementsByClassName
+// highlight Avengers - changing the color & font size
 function highlightAvengers() {
     const avengers = document.getElementsByClassName("avenger");
     for (let i = 0; i < avengers.length; i++) {
-        avengers[i].style.color = "red"; // Changing text color to red
-        avengers[i].style.fontSize = "22px"; // Changing font size
+        avengers[i].style.color = "red"; 
+        avengers[i].style.fontSize = "22px"; 
     }
 }
 
-// Function to change the image source using getElementById
+// change image 
 function changeImage() {
     const img = document.getElementById("avenger-img");
-    img.src = "assets/avengers-image2.png"; // Update with a new Avengers image
+    img.src = "assets/avengers-image2.png"; 
     img.alt = "New Avengers Image";
 }
 
-// Function to add a new Avenger to the list
+// Add a new node
 function addAvenger() {
     const avengerList = document.getElementById("avenger-list");
 
-    // Create new list item
     const newAvenger = document.createElement("li");
-    newAvenger.className = "avenger"; // Add class to new element
-    newAvenger.style.color = "white"; // Set the color of new avenger
+    newAvenger.className = "avenger"; 
+    newAvenger.style.color = "white"; 
 
-    // Create text node and append it to the new li element
     const avengerName = document.createTextNode("Spider-Man");
     newAvenger.appendChild(avengerName);
 
-    // Append new avenger to the list
+    // Append new node
     avengerList.appendChild(newAvenger);
 }
